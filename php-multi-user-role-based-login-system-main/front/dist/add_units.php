16c1d4bcf9cfa110201ca4942966cb6bc8ba8360
Sardar Uzair <110556150+sardaruzair1@users.noreply.github.com>
2022-09-14 02:02:24 -0700

    this is the 2.0

---
+++ php-multi-user-role-based-login-system-main/front/dist/add_units.php
@@ -0,0 +1,40 @@
+<div class="container">
+<h1 class="mt-4">Add Units</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Add-Units</li>
+                        </ol>
+</div>
+
+<div class="container">
+<form action="" method="post">
+<form class="form-inline">
+    <div class="form-group">
+        <label for=""></label>
+        <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+        <small id="helpId" class="text-muted">Help text</small>
+    </div>
+</form>
+<form class="form-inline">
+    <div class="form-group">
+        <label for=""></label>
+        <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+        <small id="helpId" class="text-muted">Help text</small>
+    </div>
+</form>
+<form class="form-inline">
+    <div class="form-group">
+        <label for=""></label>
+        <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+        <small id="helpId" class="text-muted">Help text</small>
+    </div>
+</form>
+<form class="form-inline">
+    <div class="form-group">
+        <label for=""></label>
+        <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+        <small id="helpId" class="text-muted">Help text</small>
+    </div>
+</form>
+<input type="submit" value="Submit" class="btn btn-info float-end">
+</form>
+</div>
---
+++ php-multi-user-role-based-login-system-main/front/dist/units.php
@@ -0,0 +1,41 @@
+<div class="container">
+<h1 class="mt-4">Units</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Units</li>
+                        </ol>
+</div>
+
+<div class="container">
+    <a href="?add_units=true" class="btn btn-primary float-end">Add Units</a>
+    <table class="table table-striped table-inverse">
+        <thead class="thead-inverse">
+            <tr>
+                <th>Id</th>
+                <th>Device</th>
+                <th>Watt</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td scope="row">01</td>
+                    <td>Fan</td>
+                    <td>50 W</td>
+                </tr>
+                <tr>
+                    <td scope="row">02</td>
+                    <td>Iron</td>
+                    <td>100 W</td>
+                </tr>
+                <tr>
+                    <td scope="row">03</td>
+                    <td>Computer</td>
+                    <td>120W</td>
+                </tr>
+                <tr>
+                    <td scope="row">04</td>
+                    <td>Washing Machine</td>
+                    <td>240W</td>
+                </tr>
+            </tbody>
+    </table>
+</div>
